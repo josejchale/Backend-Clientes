03c28e4ce4c141728d5ef2fa93288b25623f6c08
Jose Chale <chalejosejuan@gmail.com>
2025-05-07 13:35:42 -0600

    fix(db): se cambiaron los valores de conection para el deploy

--- db.js
+++ db.js
@@ -2,10 +2,11 @@ const mysql = require ("mysql2");
 
 
 const conection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "api_clientes"
+    host: "dpg-d0dr93ndiees73a7i890-a",
+    user: "josec",
+    password: "oyTURJiErxQBYhQuw52SQes9KjLqvLCM",
+    database: "api_clientes",
+    port:'5432'
 });
 
 conection.connect((err)=>{
